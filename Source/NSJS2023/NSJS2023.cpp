97f8452d770df2b300a574150e2a9d1ca395441c
Viktor Blomme <viktor.blomme@hotmail.com>
2023-08-25 17:43:57 +0200

    Convert to C++ project

---
+++ Source/NSJS2023/NSJS2023.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NSJS2023.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NSJS2023, "NSJS2023" );
